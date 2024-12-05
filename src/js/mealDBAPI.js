07585ecd3d168754224f8e7b8b4be5a630d1e425
Joseph Martin <se.josephmartin@gmail.com>
2024-12-05 16:06:26 +0800

    refactor: make it more  organize and modular

--- src/js/mealDBAPI.js
+++ src/js/mealDBAPI.js
@@ -1,15 +1,46 @@
 document.addEventListener('DOMContentLoaded', function () {
-	function displayRecipeInformation(recipeId) {
-		fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`)
-			.then((response) => response.json())
-			.then((data) => {
-				if (data.meals) {
-					const recipe = data.meals[0];
+	// API calls
+	const fetchRecipesBySearchTerm = async (searchTerm) => {
+		const response = await fetch(
+			`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`
+		);
+		return await response.json();
+	};
 
-					const recipeDetailsContainer = document.createElement('div');
-					recipeDetailsContainer.className = 'recipe-details-container';
+	const fetchRecipesByFilter = async (filterType, filterValue) => {
+		return fetch(
+			`https://www.themealdb.com/api/json/v1/1/filter.php?${filterType}=${filterValue}`
+		).then((response) => response.json());
+	};
 
-					recipeDetailsContainer.innerHTML = `
+	const fetchRecipeById = async (recipeId) => {
+		return fetch(
+			`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`
+		).then((response) => response.json());
+	};
+
+	// Function to generate ingredient list HTML in recipe.html
+	function generateIngredientList(recipe) {
+		let ingredientList = '';
+		for (let i = 1; i <= 20; i++) {
+			const ingredient = recipe[`strIngredient${i}`];
+			const measure = recipe[`strMeasure${i}`];
+			if (ingredient) {
+				ingredientList += `<li>${
+					measure ? measure.trim() + ' ' : ''
+				}${ingredient}</li>`;
+			} else {
+				break; // Stop loop if no more ingredients
+			}
+		}
+		return ingredientList;
+	}
+
+	const createRecipeDetailsContainer = (recipe) => {
+		const recipeDetailsContainer = document.createElement('div');
+		recipeDetailsContainer.className = 'recipe-details-container';
+
+		recipeDetailsContainer.innerHTML = `
 		<div class="recipe-bg">
 			<img
 				src="${recipe.strMealThumb}"
@@ -66,48 +97,27 @@ document.addEventListener('DOMContentLoaded', function () {
 			>Go back</a
 		>
 					`;
-					document.body.appendChild(recipeDetailsContainer);
-				}
-			});
-	}
 
-	// Function to generate ingredient list HTML in recipe.html
-	function generateIngredientList(recipe) {
-		let ingredientList = '';
-		for (let i = 1; i <= 20; i++) {
-			const ingredient = recipe[`strIngredient${i}`];
-			const measure = recipe[`strMeasure${i}`];
-			if (ingredient) {
-				ingredientList += `<li>${
-					measure ? measure.trim() + ' ' : ''
-				}${ingredient}</li>`;
-			} else {
-				break; // Stop loop if no more ingredients
+		document.body.appendChild(recipeDetailsContainer);
+	};
+
+	function displayRecipeInformation(recipeId) {
+		fetchRecipeById(recipeId).then((data) => {
+			if (data.meals) {
+				const recipe = data.meals[0];
+				createRecipeDetailsContainer(recipe);
 			}
-		}
-		return ingredientList;
+		});
 	}
 
-	// Function to fetch and display recipes based on search term
-	function fetchAndDisplayRecipes(searchTerm) {
-		const recipeContainer = document.getElementById('recipe-container');
-
-		fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchTerm}`)
-			.then((response) => response.json())
-			.then((data) => {
-				recipeContainer.innerHTML = ''; // Clear previous results
-				if (data.meals) {
-					document.getElementById(
-						'recipe-count'
-					).textContent = `${data.meals.length} Recipes Found`;
-					data.meals.forEach((meal) => {
-						const mealDiv = document.createElement('div');
-						mealDiv.className = 'meal';
-						mealDiv.dataset.id = meal.idMeal;
-						mealDiv.innerHTML = `
+	const createRecipeCards = (meal) => {
+		const mealDiv = document.createElement('div');
+		mealDiv.className = 'meal';
+		mealDiv.dataset.id = meal.idMeal;
+		mealDiv.innerHTML = `
                             <img src="${meal.strMealThumb}" alt="${
-							meal.strMeal
-						}">
+			meal.strMeal
+		}">
                             <div class="recipe-info">
                                 <h3>${meal.strMeal}</h3>
 																<span>${meal.strTags ? meal.strTags : 'No Tags'}</span><br />
@@ -116,77 +126,51 @@ document.addEventListener('DOMContentLoaded', function () {
 																}">View Recipe</button>
                             </div>
                         `;
-						recipeContainer.appendChild(mealDiv);
-						document
-							.getElementById('noRecipe-message')
-							.classList.remove('message');
-					});
-				} else {
-					document.getElementById('recipe-count').textContent =
-						'No Recipes Found';
-					document.getElementById('noRecipe-message').textContent =
-						'No results found';
-				}
-			});
-	}
+		return mealDiv;
+	};
 
-	// Handle when the button of the recipe is clicked.
-	document.addEventListener('click', (event) => {
-		if (event.target.matches('.view-recipe')) {
-			const recipeId = event.target.dataset.id;
-			if (recipeId) {
-				window.open(`recipe.html?view-recipe=${recipeId}`, '_self');
+	// Function to fetch and display recipes based on search term
+	function fetchAndDisplayRecipes(searchTerm) {
+		const recipeContainer = document.getElementById('recipe-container');
+		const recipeCount = document.getElementById('recipe-count');
+		const noRecipeMessage = document.getElementById('noRecipe-message');
+
+		fetchRecipesBySearchTerm(searchTerm).then((data) => {
+			recipeContainer.innerHTML = ''; // Clear previous results
+			if (!data.meals) {
+				recipeCount.innerHTML = `0 Recipes Found`;
+				noRecipeMessage.textContent = '0 Recipes Found';
 			} else {
-				console.error('No recipeId found for the clicked recipe button.');
+				recipeCount.innerHTML = `${data.meals.length} Recipes Found`;
+				data.meals.forEach((meal) => {
+					const mealDiv = createRecipeCards(meal);
+
+					recipeContainer.appendChild(mealDiv);
+					noRecipeMessage.classList.remove('message');
+				});
 			}
-		}
-	});
+		});
+	}
 
 	// For displaying the recipe details once the recipeId was found
 	const urlParam = new URLSearchParams(window.location.search);
 	const recipeId = urlParam.get('view-recipe');
-
 	if (recipeId) {
 		displayRecipeInformation(recipeId);
 	}
+
 	// Check if a search term is present in the URL
 	const urlParams = new URLSearchParams(window.location.search);
 	const searchTerm = urlParams.get('search');
 	if (searchTerm) {
-		// Fetch and display recipes if search term is present
 		fetchAndDisplayRecipes(searchTerm);
 	}
 
-	// display the recipes whenever the user pressed the 'enter' key
-	document.getElementById('search').addEventListener('keydown', (event) => {
-		if (event.key === 'Enter') {
-			const userInput = document.getElementById('search').value.trim();
-			if (userInput) {
-				// Navigate to the search results page in the same tab
-				window.location.href = `displayRecipe.html?search=${encodeURIComponent(
-					userInput
-				)}`;
-			}
-			document.getElementById('search').value = ''; // remove previous search value once submitted.
-		}
-	});
+	// Event listeners
 
-	// display the recipes whenever the user clicks a search button
-	document.getElementById('search-btn').addEventListener('click', () => {
-		const userInput = document.getElementById('search').value.trim();
-		if (userInput) {
-			// Navigate to the search results page in the same tab
-			window.location.href = `displayRecipe.html?search=${encodeURIComponent(
-				userInput
-			)}`;
-		}
-		document.getElementById('search').value = ''; // remove previous search value once submitted.
-	});
-
-	console.log(document.querySelector('.filter-options'));
-	// Get the parent element containing all filter options
+	// Filter recipes by category, area, or ingredient
 	const filterOptions = document.querySelector('.filter-options');
-	// Attach click event listener to the parent element
+
 	filterOptions.addEventListener('click', (event) => {
 		if (event.target.tagName === 'SPAN') {
 			// Find the closest parent div with the class representing the filter type
@@ -197,45 +181,77 @@ document.addEventListener('DOMContentLoaded', function () {
 				const filterType = filterDiv.classList[0].charAt(0);
 				const filterValue = event.target.textContent.trim();
 
+				const recipeCount = document.getElementById('recipe-count');
 				const recipeContainer = document.getElementById('recipe-container');
-				// Construct the API URL based on filter type and value
-				const apiUrl = `https://www.themealdb.com/api/json/v1/1/filter.php?${filterType}=${filterValue}`;
-				// Fetch data from the API
-				fetch(apiUrl)
-					.then((response) => response.json())
-					.then((data) => {
-						recipeContainer.innerHTML = ''; // Clear previous results
-						if (data.meals) {
-							document.getElementById(
-								'recipe-count'
-							).textContent = `${data.meals.length} Recipes Found`;
-							data.meals.forEach((meal) => {
-								const mealDiv = document.createElement('div');
-								mealDiv.className = 'meal';
-								mealDiv.dataset.id = meal.idMeal;
-								mealDiv.innerHTML = `
+				const recipeMessage = document.getElementById('noRecipe-message');
+
+				fetchRecipesByFilter(filterType, filterValue).then((data) => {
+					recipeContainer.innerHTML = ''; // Clear previous results
+					if (data.meals) {
+						recipeCount.textContent = `${data.meals.length} Recipes Found`;
+
+						data.meals.forEach((meal) => {
+							const mealDiv = document.createElement('div');
+							mealDiv.className = 'meal';
+							mealDiv.dataset.id = meal.idMeal;
+							mealDiv.innerHTML = `
 																<img  loading="lazy" src="${meal.strMealThumb}" alt="${meal.strMeal}">
 																<div class="recipe-info">
 																		<h3>${meal.strMeal}</h3>
 																		<button type="button" class="view-recipe" data-id="${meal.idMeal}">View Recipe</button>
 																</div>
 														`;
-								recipeContainer.appendChild(mealDiv);
-								document.getElementById('noRecipe-message').textContent = '';
-							});
-						} else {
-							document.getElementById('recipe-count').textContent =
-								'No Recipes Found';
-							document.getElementById('noRecipe-message').textContent =
-								'No Recipes Found';
-							document
-								.getElementById('noRecipe-message')
-								.classList.add('message');
-						}
-					});
+							recipeContainer.appendChild(mealDiv);
+							recipeMessage.textContent = '';
+						});
+					} else {
+						recipeCount.textContent = 'No Recipes Found';
+						recipeMessage.textContent = 'No Recipes Found';
+
+						recipeMessage.classList.add('message');
+					}
+				});
 			} else {
 				console.error('Parent div with filter type class not found.');
 			}
 		}
 	});
+});
+
+// display the recipes whenever the user pressed the 'enter' key
+document.getElementById('search').addEventListener('keydown', (event) => {
+	if (event.key === 'Enter') {
+		const userInput = document.getElementById('search').value.trim();
+		if (userInput) {
+			// Navigate to the search results page in the same tab
+			window.location.href = `displayRecipe.html?search=${encodeURIComponent(
+				userInput
+			)}`;
+		}
+		document.getElementById('search').value = ''; // remove previous search value once submitted.
+	}
+});
+
+// display the recipes whenever the user clicks a search button
+document.getElementById('search-btn').addEventListener('click', () => {
+	const userInput = document.getElementById('search').value.trim();
+	if (userInput) {
+		// Navigate to the search results page in the same tab
+		window.location.href = `displayRecipe.html?search=${encodeURIComponent(
+			userInput
+		)}`;
+	}
+	document.getElementById('search').value = ''; // remove previous search value once submitted.
+});
+
+// Handle when the button of the recipe is clicked.
+document.addEventListener('click', (event) => {
+	if (event.target.matches('.view-recipe')) {
+		const recipeId = event.target.dataset.id;
+		if (recipeId) {
+			window.open(`recipe.html?view-recipe=${recipeId}`, '_blank');
+		} else {
+			console.error('No recipeId found for the clicked recipe button.');
+		}
+	}
 });
